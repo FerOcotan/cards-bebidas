44385a11be9cd1dbda17f634afb93e1d63158173
Melvin Fernando Ocotan Morales <melvin.ocotan@catolica.edu.sv>
2025-04-16 11:07:03 -0600

    favorites menu andd star notification

--- src/Views/FavoritesPage.tsx
+++ src/Views/FavoritesPage.tsx
@@ -1,15 +1,29 @@
-
+import { useMemo } from "react"
+import DrinkCard from "../components/DrinkCard"
+import { useAppStore } from "../stores/useAppStore"
 
 export default function FavoritesPage() {
+  const favorites = useAppStore((state) => state.favorites)
+  const hasFavorites = useMemo(() => favorites.length , [favorites])
+
   return (
     <>
-     
-     
-      
-      <div className="container mx-auto py-16">
-        <h1 className="text-3xl font-bold">Favoritos</h1>
-        <p className="text-gray-500 mt-2">Aqui você pode ver os seus filmes favoritos.</p>
-      </div>
-     </>
+      <h1 className="text-6xl font-extrabold">Favoritos</h1>
+
+      {hasFavorites ? (
+        <div className="grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 my-10 gap-10">
+          {favorites.map( (drink: { idDrink: string; strDrink: string; strDrinkThumb: string }) => (
+              <DrinkCard
+                key={drink.idDrink}
+                drink={drink}
+              />
+          ) )}
+        </div>
+      ) : (
+        <p className="my-10 text-center text-2xl">
+          Los favoritos se mostrarán aquí
+        </p>
+      )}
+    </>
   )
 }
--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -7,6 +7,8 @@ export default function Modal() {
   useAppStore((state) => state.modal)
   const modal = useAppStore((state) => state.modal)
   const selectedRecipe = useAppStore((state) => state.selectedRecipe);
+  const handleClickFavorite = useAppStore((state) => state.handleClickFavorite)
+  const favoriteExists = useAppStore((state) => state.favoriteExists)
 
   const renderIngredients = () => {
     const ingredients = Object.entries(selectedRecipe).filter(([key, value]) => {
@@ -48,9 +50,8 @@ export default function Modal() {
                 leaveTo="opacity-0 scale-95"
               >
                 <DialogPanel className="relative transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-2xl sm:p-6" >
-                  <DialogTitle as="h3" className="text-gray-300 text-4xl font-extrabold my-5 text-center">
-                      {selectedRecipe.strDrink}
-                  </DialogTitle>
+                  <DialogTitle as="h3" className="text-gray-900 text-4xl font-extrabold my-5 text-center">
+                      {selectedRecipe.strDrink}                  </DialogTitle>
                   <img src={selectedRecipe.strDrinkThumb} 
                    alt={`Imagen de ${selectedRecipe.strDrink}`}
                     className='mx-auto mb-5 rounded-lg shadow-lg'
@@ -67,6 +68,25 @@ export default function Modal() {
                       {selectedRecipe.strInstructions}
 
                     </p>
+
+                    <div className='flex justify-center gap-4 my-5'>
+                      <button
+                        onClick={closeModal}
+                        type="button"
+                        className='mt-5 inline-flex justify-center rounded-md border border-transparent bg-gray-500 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-gray-600 focus:outline-none focus:ring-2 focus:gray-orange-500 focus:ring-offset-2 sm:mt-0 sm:w-auto sm:text-sm'
+                          >
+                        Cerrar
+                      </button>
+
+                      <button 
+                        onClick={() => handleClickFavorite(selectedRecipe)}
+                        className='mt-5 inline-flex justify-center rounded-md border border-transparent bg-orange-500 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-orange-600 focus:outline-none focus:ring-2 focus:ring-orange-500 focus:ring-offset-2 sm:mt-0 sm:w-auto sm:text-sm'
+                        type="button"
+                        >
+                        {favoriteExists(selectedRecipe.idDrink) ? 'Eliminar de Favoritos' : 'Agregar a Favoritos'}
+                        
+                      </button>
+                    </div>
                   </DialogTitle>
                 </DialogPanel>
               </TransitionChild>
